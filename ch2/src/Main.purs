dc838d037a700bc5f00a621ebecfe87cf97f99db
George Wilson <george@wils.online>
2019-11-23 17:29:36 +1000

    chapter 2

--- ch2/src/Main.purs
+++ ch2/src/Main.purs
@@ -3,8 +3,15 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console (logShow)
+import Math (pi, sqrt)
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main =
+  logShow (diagonal 3.0 4.0)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w*w + h*h)
+
+circleArea :: Number -> Number
+circleArea r = pi * r * r
